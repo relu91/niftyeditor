d777927f476d846a2459528eab17d132c1e43adf
relu91 <relu.cri@gmail.com>
2014-04-18 19:31:11 +0200

    Change ClassUtils now it works also with jar. It's a temporary implementation but it works.

--- src/jada/ngeditor/model/utils/ClassUtils.java
+++ src/jada/ngeditor/model/utils/ClassUtils.java
@@ -6,80 +6,123 @@ package jada.ngeditor.model.utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
-import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
- *
+ * <b>Notice<\b> This class is an early implementation , could change every time. 
  * @author cris
  */
 public class ClassUtils {
-    
-    public static Class[] getClasses(String packageName) throws ClassNotFoundException, IOException{
-        return ClassUtils.getClasses(packageName, NullPredicate.instance);
-    }
-    /**
- * Scans all classes accessible from the context class loader which belong to the given package and subpackages.
- *
- * @param packageName The base package
- * @return The classes
- * @throws ClassNotFoundException
- * @throws IOException
- */
-public static Class[] getClasses(String packageName,Predicate<Class> predicate)
-        throws ClassNotFoundException, IOException {
-    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-    assert classLoader != null;
-    String path = packageName.replace('.', '/');
-    Enumeration<URL> resources = classLoader.getResources(path);
-    List<File> dirs = new ArrayList<File>();
-    while (resources.hasMoreElements()) {
-        URL resource = resources.nextElement();
-        dirs.add(new File(resource.getFile()));
-    }
-    ArrayList<Class> classes = new ArrayList<Class>();
-    for (File directory : dirs) {
-        classes.addAll(findClasses(directory, packageName,predicate));
-    }
-    return classes.toArray(new Class[classes.size()]);
-}
+	private static void log(String msg) {
+		//System.out.println("ClassDiscovery: " + msg);	
+	}
 
-/**
- * Recursive method used to find all classes in a given directory and subdirs.
- *
- * @param directory   The base directory
- * @param packageName The package name for classes found inside the base directory
- * @return The classes
- * @throws ClassNotFoundException
- */
-private static List<Class> findClasses(File directory, String packageName, Predicate<Class> predicate) throws ClassNotFoundException {
-    List<Class> classes = new ArrayList<Class>();
-    if (!directory.exists()) {
-        return classes;
-    }
-    File[] files = directory.listFiles();
-    for (File file : files) {
-        if (file.isDirectory()) {
-            assert !file.getName().contains(".");
-            classes.addAll(findClasses(file, packageName + "." + file.getName(),predicate));
-        } else if (file.getName().endsWith(".class")) {
-            Class<?> newClass = Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6));
-            if(predicate.apply(newClass)){
-                classes.add(newClass);
-            }
-        }
-    }
-    return classes;
-}
+	private static Class<?> loadClass(String className) {
+		try {
+			return Class.forName(className);
+		} 
+		catch (ClassNotFoundException e) {
+			throw new RuntimeException("Unexpected ClassNotFoundException loading class '" + className + "'");
+		}
+	}
+
+	private static void processDirectory(File directory, String pkgname, ArrayList<Class<?>> classes) {
+		log("Reading Directory '" + directory + "'");
+		// Get the list of the files contained in the package
+		String[] files = directory.list();
+		for (int i = 0; i < files.length; i++) {
+			String fileName = files[i];
+			String className = null;
+			// we are only interested in .class files
+			if (fileName.endsWith(".class")) {
+				// removes the .class extension
+				className = pkgname + '.' + fileName.substring(0, fileName.length() - 6);
+			}
+			log("FileName '" + fileName + "'  =>  class '" + className + "'");
+			if (className != null) {
+				classes.add(loadClass(className));
+			}
+			File subdir = new File(directory, fileName);
+			if (subdir.isDirectory()) {
+				processDirectory(subdir, pkgname + '.' + fileName, classes);
+			}
+		}
+	}
+
+	private static void processJarfile(URL resource, String pkgname, ArrayList<Class<?>> classes) {
+		String relPath = pkgname.replace('.', '/');
+		String resPath = resource.getPath();
+		String jarPath = resPath.replaceFirst("[.]jar[!].*", ".jar").replaceFirst("file:", "");
+		log("Reading JAR file: '" + jarPath + "'");
+		JarFile jarFile;
+		try {
+			jarFile = new JarFile(jarPath);         
+		} catch (IOException e) {
+			throw new RuntimeException("Unexpected IOException reading JAR File '" + jarPath + "'", e);
+		}
+		Enumeration<JarEntry> entries = jarFile.entries();
+		while(entries.hasMoreElements()) {
+			JarEntry entry = entries.nextElement();
+			String entryName = entry.getName();
+			String className = null;
+			if(entryName.endsWith(".class") && entryName.startsWith(relPath) && entryName.length() > (relPath.length() + "/".length())) {
+				className = entryName.replace('/', '.').replace('\\', '.').replace(".class", "");
+			}
+			log("JarEntry '" + entryName + "'  =>  class '" + className + "'");
+			if (className != null) {
+				classes.add(loadClass(className));
+			}
+		}
+	}
+
+	public static ArrayList<Class<?>> getClassesForPackage(String pkg) {
+		ArrayList<Class<?>> classes = new ArrayList<Class<?>>();
+		String pkgname = pkg;
+		String relPath = pkgname.replace('.', '/');
+		// Get a File object for the package
+		URL resource = ClassLoader.getSystemClassLoader().getResource(relPath);
+		if (resource == null) {
+			throw new RuntimeException("Unexpected problem: No resource for " + relPath);
+		}
+		log("Package: '" + pkgname + "' becomes Resource: '" + resource.toString() + "'");
+
+		resource.getPath();
+		if(resource.toString().startsWith("jar:")) {
+			processJarfile(resource, pkgname, classes);
+		} else {
+			processDirectory(new File(resource.getPath()), pkgname, classes);
+		}
+
+		return classes;
+	}
+
 
 public static void main(String[] args) throws IOException, ClassNotFoundException{
-    Class [] res = ClassUtils.getClasses("jada.ngeditor.model",NullPredicate.instance);
-    for(Class c : res){
+        ArrayList<Class<?>> classesForPackage = ClassUtils.getClassesForPackage("jada.ngeditor.model");
+    for(Class c : classesForPackage){
         System.out.println(c.getName());
     }
 }
+
+ public static Class[] getClasses(String pkg){
+     return getClasses(pkg,new NullPredicate());
+ }
+
+    public static Class[] getClasses(String pkg, Predicate<Class> predicate) {
+        ArrayList<Class<?>> result = new ArrayList<Class<?>>();
+        ArrayList<Class<?>> classesForPackage = ClassUtils.getClassesForPackage(pkg);
+        for(Class c : classesForPackage){
+            if(predicate.apply(c)){
+                result.add(c);
+            }
+        }
+        return (Class[]) result.toArray(new Class[0]);
+    }
 
 public static class NullPredicate implements Predicate<Class> {
        public static NullPredicate instance = new NullPredicate();
